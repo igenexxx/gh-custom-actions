ac83c1a6d815bbcbdad1a22086407bf51282b632
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-12-10 19:44:01 +0200

    master: updated workflow

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,30 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
-function run() {
+
+const inputs = {
+    bucket: {
+        name: 'bucket',
+        required: true
+    },
+    bucketRegion: {
+        name: 'bucket-region',
+        required: true
+    },
+    distFolder: {
+        name: 'dist-folder',
+        required: true
+    }
+}
+
+async function run() {
+    const { bucket, bucketRegion, distFolder } = Object.entries(inputs).reduce((acc, [key, {name, required}]) => ({
+        ...acc,
+        [key]: core.getInput(name, {required})
+    }), {});
+
+    const s3Uri = `s3://${bucket}`;
+    await exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Hello from JS actions!');
 }
 
