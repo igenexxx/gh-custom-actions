ae4904cdfc7fbfcdd375316deba3244903fc4b02
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-12-11 13:15:01 +0200

    master: fixed variable

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -7,18 +7,18 @@ from botocore.config import Config
 def run():
     bucket = os.environ['INPUT_BUCKET']
     bucket_region = os.environ['INPUT_BUCKET-REGION']
-    dist_folder = os.environ['INPUT_DIST-FOLDER']
+    dist_dir = os.environ['INPUT_DIST-DIR']
 
     configuration = Config(region_name=bucket_region)
 
     s3_client = boto3.client('s3', config=configuration)
 
-    for root, subdirs, files in os.walk(dist_folder):
+    for root, subdirs, files in os.walk(dist_dir):
         for file in files:
             s3_client.upload_file(
                 os.path.join(root, file),
                 bucket,
-                os.path.join(root, file).replace(dist_folder + '/', ''),
+                os.path.join(root, file).replace(dist_dir + '/', ''),
                 ExtraArgs={"ContentType": mimetypes.guess_type(file)[0]}
             )
 
